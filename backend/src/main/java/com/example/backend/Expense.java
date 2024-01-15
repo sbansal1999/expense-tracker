37865dbd0a202c97a8d814aa826ef0f338a33b6e
Satyam Bansal <sbansal1999@gmail.com>
2024-01-15 21:39:36 +0530

    Add Expense model

---
+++ backend/src/main/java/com/example/backend/Expense.java
@@ -0,0 +1,58 @@
+package com.example.backend;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Expense {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+    private String title;
+    private String amount;
+    private String description;
+
+    public Expense(String title, String amount, String description) {
+        this.title = title;
+        this.amount = amount;
+        this.description = description;
+    }
+
+    public Expense() {
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
