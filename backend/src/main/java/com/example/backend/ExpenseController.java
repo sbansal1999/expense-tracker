b512b6a99407e50306938202f0ccb12a0156c459
Satyam Bansal <sbansal1999@gmail.com>
2024-01-17 20:25:43 +0530

    Allow Cross-Origin request

--- backend/src/main/java/com/example/backend/ExpenseController.java
+++ backend/src/main/java/com/example/backend/ExpenseController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping(path = "expense")
 public class ExpenseController {
     @Autowired
